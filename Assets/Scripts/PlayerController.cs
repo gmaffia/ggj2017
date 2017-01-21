6e7248f054e8e42e08fcd481c84ca7a594515443
Clovers! <CloversDevs@gmail.com>
2017-01-21 19:52:47 -0300

    Arreglando Todo, + Animaciones Girl

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : NetworkBehaviour
 
 	// Use this for initialization
 	void Start () {
-		
+		sprite = GetComponent<SpriteRenderer> ();
 		rBody = GetComponent<Rigidbody2D> ();
         if (isServer)
         {
@@ -55,18 +55,70 @@ public class PlayerController : NetworkBehaviour
 		float y = Input.GetAxis("Vertical") * Time.deltaTime * this.velocity;
 		rBody.MovePosition (rBody.position + new Vector2(x, y));
 	}
-
+	enum facingDirection {Front,Left,Right,Up}
+	Vector3 lastPostion = Vector3.zero;
+	SpriteRenderer sprite;
+	void Animate(){
+		if (isLocalPlayer) {
+			float x = Input.GetAxis("Horizontal");
+			float y = Input.GetAxis("Vertical");
+			if (x!=0) {				
+				charAnimator.SetBool ("Walking", true);
+				charAnimator.SetBool ("FacingSide",true);
+				charAnimator.SetBool ("FacingFront",false);
+				charAnimator.SetBool ("FacingBack",false);
+				if (x < 0) {
+					sprite.flipX = true;
+				} else {
+					sprite.flipX = false;
+				}
+			} else {
+				if (y != 0) {
+					sprite.flipX = false;
+					charAnimator.SetBool ("FacingSide", false);
+					charAnimator.SetBool ("Walking", true);
+					if (y > 0) {
+						charAnimator.SetBool ("FacingFront", false);
+						charAnimator.SetBool ("FacingBack", true);
+					} else {
+						charAnimator.SetBool ("FacingFront", true);
+						charAnimator.SetBool ("FacingBack", false);
+					}
+				} else {
+					charAnimator.SetBool ("Walking", false);
+				}
+			}
+		} else {
+			if (transform.position.x != lastPostion.x) {
+				//Moving H
+				if (transform.position.x > lastPostion.x) {
+					sprite.flipX = true;
+					//Moving left
+				}
+				if (transform.position.x < lastPostion.x) {
+					sprite.flipX = false;
+					//MovingRight
+				}
+			} else {
+				if (transform.position.y != lastPostion.y) {
+					sprite.flipX = false;
+					//Moving V
+					if (transform.position.y > lastPostion.y) {
+						//Moving Up
+					}
+					if (transform.position.y < lastPostion.y) {
+						//Moving Down
+					}
+				} else {
+					//Not Moving
+				}
+			}
+		}
+	}
 	// Update is called once per frame
 	void Update () {
-
-        if (!isLocalPlayer)
-        {
-            return;
-        }
-
-        
-
-        if (Input.GetKeyDown(KeyCode.Space))
+		Animate ();
+		if (isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
         {
             CmdDisclosePosition();
         }
