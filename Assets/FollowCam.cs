b872173e0c9317805833ede17055c53a92c55f84
Clovers! <CloversDevs@gmail.com>
2017-01-21 16:06:37 -0300

    Camera Following, Shadow Activating

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+	
+	Transform target = null;
+
+	public void SetTarget(Transform target){
+		this.target = target;
+	}
+
+	void LateUpdate () {
+		if (target != null) {
+			transform.position = new Vector3 (target.position.x, target.position.y, transform.position.z);
+		}
+	}
+}
