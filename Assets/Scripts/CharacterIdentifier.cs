47cc80370af0c27853755a77525e07d831cb4604
Gabriel Maffia <gmaffia@leanbirds.com>
2017-01-22 13:51:19 -0300

    Adds Player 1's steps

--- Assets/Scripts/CharacterIdentifier.cs
+++ Assets/Scripts/CharacterIdentifier.cs
@@ -15,20 +15,40 @@ public class CharacterIdentifier : NetworkBehaviour
 		if(isLocalPlayer)
 		{
 			FindObjectOfType<GameManager>().currentPlayer = newPlayerId;
-		}
+            Debug.Log("Locally, the player is " + newPlayerId);
+            AkSoundEngine.SetState("player1_state", "is" + newPlayerId);
+        }
 
-		Animator charAnimator = GetComponent<Animator> ();
-		//FindObjectOfType<GameManager> ().currentPlayer
-		switch (playerId) {
-			case ("Alice"):
-				charAnimator.SetTrigger ("Alice");
+        int[] animationClips = new int[3];
+        Animator charAnimator = GetComponent<Animator> ();
+        //FindObjectOfType<GameManager> ().currentPlayer
+        switch (playerId) {
+            case ("Alice"):
+                charAnimator.SetTrigger("Alice");
+                animationClips = new int[] { 1, 3, 5};
 				break;
 			case ("Bob"):
 				charAnimator.SetTrigger ("Bob");
-				break;
+                animationClips = new int[] { 8, 10, 12 };
+                break;
 			case ("Slasher"):
 				charAnimator.SetTrigger ("Slasher");
-				break;
+                animationClips = new int[] { 13, 15, 17 };
+                break;
 		}
-	}
+
+        AnimationEventSoundPlayer animationSoundPlayer = gameObject.GetComponentInChildren<AnimationEventSoundPlayer>();
+        Debug.Log("Animation event player?" + animationSoundPlayer.ToString());
+        if (animationSoundPlayer != null)
+        {
+            for(int i = 0; i < animationClips.Length; i++)
+            {
+                animationSoundPlayer.addSoundEvent(animationClips[i], 0.2f, "player1_step");                
+                animationSoundPlayer.addSoundEvent(animationClips[i], 0.5f, "player1_step");
+                animationSoundPlayer.addSoundEvent(animationClips[i], 0.8f, "player1_step");
+                animationSoundPlayer.addSoundEvent(animationClips[i], 1.0f, "player1_step");
+            }
+        }
+
+    }
 }
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,105 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMBIENT = 1562304622U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAYER1_PANTING = 4186058215U;
+        static const AkUniqueID PLAYER1_STEP = 3235708380U;
+        static const AkUniqueID PLAYER2_STEP = 2345449353U;
+        static const AkUniqueID PLAYER3_STEP = 2062355390U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYER1_STATE
+        {
+            static const AkUniqueID GROUP = 1675162649U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ISALICE = 3354824903U;
+                static const AkUniqueID ISBOB = 2165475990U;
+                static const AkUniqueID ISSLASHER = 3013609521U;
+            } // namespace STATE
+        } // namespace PLAYER1_STATE
+
+        namespace PLAYER2_STATE
+        {
+            static const AkUniqueID GROUP = 2421122886U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ISALICE = 3354824903U;
+                static const AkUniqueID ISBOB = 2165475990U;
+                static const AkUniqueID ISSLASHER = 3013609521U;
+            } // namespace STATE
+        } // namespace PLAYER2_STATE
+
+        namespace PLAYER3_STATE
+        {
+            static const AkUniqueID GROUP = 1026818895U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ISALICE = 3354824903U;
+                static const AkUniqueID ISBOB = 2165475990U;
+                static const AkUniqueID ISSLASHER = 3013609521U;
+            } // namespace STATE
+        } // namespace PLAYER3_STATE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYER1_TO_2_DISTANCE = 2029976456U;
+        static const AkUniqueID PLAYER1_TO_3_DISTANCE = 3092056143U;
+        static const AkUniqueID PLAYER1_TO_SLASHER_DISTANCE = 3472398812U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID AMBIENT_REVERB = 3036939274U;
+        static const AkUniqueID MUSICREVERB = 1575916026U;
+        static const AkUniqueID SFXREVERB = 2943096322U;
+    } // namespace AUX_BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
