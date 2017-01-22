d5d7c3732e4f7a51f3fc54cbc25fa544bdb30b86
Gabriel Maffia <gmaffia@leanbirds.com>
2017-01-22 14:14:32 -0300

    Adds step configuration

--- Assets/Scripts/CharacterIdentifier.cs
+++ Assets/Scripts/CharacterIdentifier.cs
@@ -8,6 +8,7 @@ public class CharacterIdentifier : NetworkBehaviour
 
 	[SyncVar(hook="OnCharChange")]
     public string playerId;
+
 	void OnCharChange(string newPlayerId){
 		Debug.Log ("->>>"+newPlayerId);
 		playerId = newPlayerId;
@@ -15,62 +16,62 @@ public class CharacterIdentifier : NetworkBehaviour
 		if(isLocalPlayer)
 		{
 			FindObjectOfType<GameManager>().currentPlayer = newPlayerId;
-            Debug.Log("Locally, the player is " + newPlayerId);
-            AkSoundEngine.SetState("player1_state", "is" + newPlayerId);
         }
-
-        int[] animationClips = new int[3];
+        
         Animator charAnimator = GetComponent<Animator> ();
         //FindObjectOfType<GameManager> ().currentPlayer
-        switch (playerId) {
+        //FindObjectOfType<GameManager> ().currentPlayer
+        int[] animationClips = new int[3];
+        switch (playerId)
+        {
             case ("Alice"):
                 charAnimator.SetTrigger("Alice");
-                animationClips = new int[] { 1, 3, 5};
-				break;
-			case ("Bob"):
-				charAnimator.SetTrigger ("Bob");
+                animationClips = new int[] { 1, 3, 5 };
+                break;
+            case ("Bob"):
+                charAnimator.SetTrigger("Bob");
                 animationClips = new int[] { 8, 10, 12 };
                 break;
-			case ("Slasher"):
-				charAnimator.SetTrigger ("Slasher");
+            case ("Slasher"):
+                charAnimator.SetTrigger("Slasher");
                 animationClips = new int[] { 13, 15, 17 };
                 break;
-		}
-<<<<<<< HEAD
-	}
-	public override void OnStartClient ()
-	{
-		base.OnStartClient ();
-		Debug.Log ("Old->>>"+playerId);
-		Animator charAnimator = GetComponent<Animator> ();
-		//FindObjectOfType<GameManager> ().currentPlayer
-		switch (playerId) {
-		case ("Alice"):
-			charAnimator.SetTrigger ("Alice");
-			break;
-		case ("Bob"):
-			charAnimator.SetTrigger ("Bob");
-			break;
-		case ("Slasher"):
-			charAnimator.SetTrigger ("Slasher");
-			break;
-		}
-	}
-=======
+        }
 
         AnimationEventSoundPlayer animationSoundPlayer = gameObject.GetComponentInChildren<AnimationEventSoundPlayer>();
         Debug.Log("Animation event player?" + animationSoundPlayer.ToString());
+        Debug.Log("Locally, the player is " + playerId);
+        AkSoundEngine.SetState("player1_state", "is" + playerId);
+
         if (animationSoundPlayer != null)
         {
-            for(int i = 0; i < animationClips.Length; i++)
+            for (int i = 0; i < animationClips.Length; i++)
             {
-                animationSoundPlayer.addSoundEvent(animationClips[i], 0.2f, "player1_step");                
+                animationSoundPlayer.addSoundEvent(animationClips[i], 0.2f, "player1_step");
                 animationSoundPlayer.addSoundEvent(animationClips[i], 0.5f, "player1_step");
                 animationSoundPlayer.addSoundEvent(animationClips[i], 0.8f, "player1_step");
                 animationSoundPlayer.addSoundEvent(animationClips[i], 1.0f, "player1_step");
             }
         }
-
     }
->>>>>>> 6a0b15e2043bbdefe4f611107419972ce3f75c0a
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        Debug.Log("Old->>>" + playerId);
+        Animator charAnimator = GetComponent<Animator>();
+        //FindObjectOfType<GameManager> ().currentPlayer
+        switch (playerId)
+        {
+            case ("Alice"):
+                charAnimator.SetTrigger("Alice");
+                break;
+            case ("Bob"):
+                charAnimator.SetTrigger("Bob");
+                break;
+            case ("Slasher"):
+                charAnimator.SetTrigger("Slasher");
+                break;
+        }
+    }
 }
